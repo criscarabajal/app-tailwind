cf1929e9d507ce520dc076d80b0c790e46454cd3
cristian-carabajal <cris.carabajal93@gmail.com>
2022-07-11 13:53:42 -0300

    probando si imprime en el DOM mi array de products

--- src/components/Item.js
+++ src/components/Item.js
@@ -2,8 +2,24 @@ import React from 'react'
 
 
 
-const Item = ({id,title,price,description,category,image}) => {
+// const Item = ({id,title,price,description,category,image}) => {
+
+//   console.log(Item, "estoy recibiendo item")
     
+//   return (
+//     <div>
+//         <img src={image} alt={title}></img>
+//         <h3>{title}</h3>
+//         <span> {description} </span>
+//         <span> {id} </span>
+//         <span> {price} </span>
+//     </div>
+//   )
+// }
+
+const Item = ({product}) => {
+  const {id,title,price,description,category,image} = product;
+
   return (
     <div>
         <img src={image} alt={title}></img>
@@ -13,6 +29,8 @@ const Item = ({id,title,price,description,category,image}) => {
         <span> {price} </span>
     </div>
   )
-}
+};
+
+
 
 export default Item;
--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,25 +1,37 @@
 import React from 'react';
 import Item from './Item';
 
-const ItemList = ({items}) => {
+// const ItemList = ({items}) => {
+//   return (
+//     <div>
+//         {items.map(({id,title,price,description,category,image}) =>{
+//             console.log(items);
+
+//         return <Item 
+//             key={id}
+//             id={id}
+//             title={title}
+//             price={price}
+//             description={description}
+//             category={category}
+//             image={image} />
+
+
+//         })}
+//     </div>
+//   )
+// }
+
+const ItemList = ({ items }) => {
   return (
     <div>
-        {items.map(({id,title,price,description,category,image}) =>{
-            console.log(items);
-
-        return <Item 
-            key={items.id}
-            id={id}
-            title={title}
-            price={price}
-            description={description}
-            category={category}
-            image={image} />
-
-
-        })}
+      {items.map((item) => {
+        console.log(item);
+        return <Item key={item.id} product={item} />;
+      })}
     </div>
-  )
-}
+  );
+};
+
 
 export default ItemList
