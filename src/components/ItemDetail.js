b2a7f36ce339daaeea3f2f7c187b84d7cc4d3780
cristian-carabajal <cris.carabajal93@gmail.com>
2022-07-20 12:17:53 -0300

    agregue /cart en ItemDetail

--- src/components/ItemDetail.js
+++ src/components/ItemDetail.js
@@ -1,12 +1,13 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { Link } from 'react-router-dom';
 import ItemCount from './ItemCount';
 
 const ItemDetail = ({productDetail}) => {
-  
-  console.log(productDetail);
-  console.log(productDetail.title);
 
 const {title,price,description,image} = productDetail;
+
+const [cart, setCart] = useState(true);
+
   return (
     <div>
       <div className="card flex flex-row my-20">
@@ -18,12 +19,20 @@ const {title,price,description,image} = productDetail;
           <p className="w-2/4 my-10">{description}</p>
           <p className="text-2xl text-emerald-400">{price}</p>
           <div className="card-actions justify-start">
-            {/* <button className="btn btn-primary">Finalizar compra</button> */}
-            <ItemCount
-              stock={5}
-              initial={1}
-              onAdd={() => alert("Agregado al carrito")}
-            />
+            {cart ? (
+              <ItemCount
+                stock={5}
+                initial={1}
+                onAdd={() => {
+                  alert("Agregado al carrito");
+                  setCart(false);
+                }}
+              />
+            ) : (
+              <Link to="/cart" className="btn btn-primary">
+                Ver Carrito
+              </Link>
+            )}
           </div>
         </div>
       </div>
